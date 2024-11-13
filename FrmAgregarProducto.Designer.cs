c95405fbadb5f8e5fbdfda436e8f9417e9b7f61b
NECT7 <142763453+NECT7@users.noreply.github.com>
2024-11-13 10:15:09 -0600

    Corrección de error causado por mi FrmAgregarProducto, se añadieron clases Pedidos y DetallePedidos

--- FrmAgregarProducto.Designer.cs
+++ FrmAgregarProducto.Designer.cs
@@ -47,67 +47,75 @@ namespace Grupo4_Clave4
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(92, 38);
+            this.label1.Location = new System.Drawing.Point(184, 73);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.Size = new System.Drawing.Size(215, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre de la comida";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(92, 89);
+            this.label2.Location = new System.Drawing.Point(184, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.Size = new System.Drawing.Size(192, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Cantidad de platos";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(92, 140);
+            this.label3.Location = new System.Drawing.Point(184, 269);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.Size = new System.Drawing.Size(153, 25);
             this.label3.TabIndex = 2;
             this.label3.Text = "Precio Unitario";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(92, 184);
+            this.label4.Location = new System.Drawing.Point(184, 354);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.Size = new System.Drawing.Size(159, 25);
             this.label4.TabIndex = 3;
             this.label4.Text = "Tipo de comida";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(92, 245);
+            this.label5.Location = new System.Drawing.Point(184, 471);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.Size = new System.Drawing.Size(189, 25);
             this.label5.TabIndex = 4;
             this.label5.Text = "Horario Disponible";
             // 
             // txtNombreProducto
             // 
-            this.txtNombreProducto.Location = new System.Drawing.Point(221, 38);
+            this.txtNombreProducto.Location = new System.Drawing.Point(442, 73);
+            this.txtNombreProducto.Margin = new System.Windows.Forms.Padding(6);
             this.txtNombreProducto.Name = "txtNombreProducto";
-            this.txtNombreProducto.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreProducto.Size = new System.Drawing.Size(196, 31);
             this.txtNombreProducto.TabIndex = 5;
             // 
             // txtCantidaProducto
             // 
-            this.txtCantidaProducto.Location = new System.Drawing.Point(221, 86);
+            this.txtCantidaProducto.Location = new System.Drawing.Point(442, 165);
+            this.txtCantidaProducto.Margin = new System.Windows.Forms.Padding(6);
             this.txtCantidaProducto.Name = "txtCantidaProducto";
-            this.txtCantidaProducto.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidaProducto.Size = new System.Drawing.Size(196, 31);
             this.txtCantidaProducto.TabIndex = 6;
             // 
             // txtPrecioProducto
             // 
-            this.txtPrecioProducto.Location = new System.Drawing.Point(221, 133);
+            this.txtPrecioProducto.Location = new System.Drawing.Point(442, 256);
+            this.txtPrecioProducto.Margin = new System.Windows.Forms.Padding(6);
             this.txtPrecioProducto.Name = "txtPrecioProducto";
-            this.txtPrecioProducto.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecioProducto.Size = new System.Drawing.Size(196, 31);
             this.txtPrecioProducto.TabIndex = 7;
             // 
             // cmbTipoProducto
@@ -116,24 +124,27 @@ namespace Grupo4_Clave4
             this.cmbTipoProducto.Items.AddRange(new object[] {
             "Comida",
             "Antojito"});
-            this.cmbTipoProducto.Location = new System.Drawing.Point(221, 181);
+            this.cmbTipoProducto.Location = new System.Drawing.Point(442, 348);
+            this.cmbTipoProducto.Margin = new System.Windows.Forms.Padding(6);
             this.cmbTipoProducto.Name = "cmbTipoProducto";
-            this.cmbTipoProducto.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipoProducto.Size = new System.Drawing.Size(238, 33);
             this.cmbTipoProducto.TabIndex = 8;
             // 
             // dtDisponibleProdcuto
             // 
             this.dtDisponibleProdcuto.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtDisponibleProdcuto.Location = new System.Drawing.Point(221, 245);
+            this.dtDisponibleProdcuto.Location = new System.Drawing.Point(442, 471);
+            this.dtDisponibleProdcuto.Margin = new System.Windows.Forms.Padding(6);
             this.dtDisponibleProdcuto.Name = "dtDisponibleProdcuto";
-            this.dtDisponibleProdcuto.Size = new System.Drawing.Size(121, 20);
+            this.dtDisponibleProdcuto.Size = new System.Drawing.Size(238, 31);
             this.dtDisponibleProdcuto.TabIndex = 9;
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(95, 319);
+            this.btnAgregar.Location = new System.Drawing.Point(190, 613);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(6);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.Size = new System.Drawing.Size(150, 44);
             this.btnAgregar.TabIndex = 10;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
@@ -142,16 +153,18 @@ namespace Grupo4_Clave4
             // dtgProductos
             // 
             this.dtgProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgProductos.Location = new System.Drawing.Point(365, 38);
+            this.dtgProductos.Location = new System.Drawing.Point(730, 73);
+            this.dtgProductos.Margin = new System.Windows.Forms.Padding(6);
             this.dtgProductos.Name = "dtgProductos";
-            this.dtgProductos.Size = new System.Drawing.Size(408, 227);
+            this.dtgProductos.RowHeadersWidth = 82;
+            this.dtgProductos.Size = new System.Drawing.Size(816, 437);
             this.dtgProductos.TabIndex = 11;
             // 
             // FrmAgregarProducto
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(785, 404);
+            this.ClientSize = new System.Drawing.Size(1570, 777);
             this.Controls.Add(this.dtgProductos);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.dtDisponibleProdcuto);
@@ -164,6 +177,7 @@ namespace Grupo4_Clave4
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "FrmAgregarProducto";
             this.Text = "FrmAgregarProducto";
             ((System.ComponentModel.ISupportInitialize)(this.dtgProductos)).EndInit();
